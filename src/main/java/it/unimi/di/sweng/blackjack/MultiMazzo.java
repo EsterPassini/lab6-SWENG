e6d28b0f96de2b6de626f284271ce1fc877fa3d3
MartinWWDC <60852345+MartinWWDC@users.noreply.github.com>
2023-11-16 15:19:02 +0100

    ROSSO: multiMazzoTest non funziona

--- src/main/java/it/unimi/di/sweng/blackjack/MultiMazzo.java
+++ src/main/java/it/unimi/di/sweng/blackjack/MultiMazzo.java
@@ -1,9 +1,22 @@
 package it.unimi.di.sweng.blackjack;
 
-public class MultiMazzo  {
+import ca.mcgill.cs.stg.solitaire.cards.Card;
+import org.jetbrains.annotations.NotNull;
+
+public class MultiMazzo implements DeckInterface  {
   // TODO deve implementare DeckInterface
 
   public MultiMazzo(int numMazzi) {
     // TODO definire un costruttore che crei un mazzo composto da numMazzi mescolato casualmente
+  }
+
+  @Override
+  public @NotNull Card draw() {
+    return null;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return false;
   }
 }
