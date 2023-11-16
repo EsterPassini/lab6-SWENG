eb152d394c667262e9ec3dfa198ff581b5a47475
MartinWWDC <60852345+MartinWWDC@users.noreply.github.com>
2023-11-16 15:29:31 +0100

    VERDE: multiMazzoTest funziona

--- src/main/java/it/unimi/di/sweng/blackjack/MultiMazzo.java
+++ src/main/java/it/unimi/di/sweng/blackjack/MultiMazzo.java
@@ -1,22 +1,44 @@
 package it.unimi.di.sweng.blackjack;
 
 import ca.mcgill.cs.stg.solitaire.cards.Card;
+import ca.mcgill.cs.stg.solitaire.cards.Deck;
 import org.jetbrains.annotations.NotNull;
 
-public class MultiMazzo implements DeckInterface  {
-  // TODO deve implementare DeckInterface
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
+public class MultiMazzo implements DeckInterface  {
+  List<Deck> deckList=new ArrayList<>();
   public MultiMazzo(int numMazzi) {
-    // TODO definire un costruttore che crei un mazzo composto da numMazzi mescolato casualmente
+    for (int i = 0; i < numMazzi; i++) {
+        deckList.add(new Deck());
+    }
   }
 
   @Override
   public @NotNull Card draw() {
-    return null;
+    if (this.isEmpty()){
+      return null;
+    }
+    int randomNum = ThreadLocalRandom.current().nextInt(0, deckList.size());
+    while (deckList.get(randomNum).isEmpty()){
+      randomNum++;
+        if(randomNum>deckList.size()){
+          randomNum=0;
+        }
+    }
+    return deckList.get(randomNum).draw();
   }
 
   @Override
   public boolean isEmpty() {
-    return false;
+    for (Deck dk:deckList) {
+      if(!dk.isEmpty()){
+        return false;
+      }
+    }
+    return true;
+
   }
 }
